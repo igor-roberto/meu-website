2111228ebae0e3a09612273fae7c7974c538dd5d
Igor roberto <igorroberto1962@hotmail.com>
2024-01-01 13:22:56 -0300

    Novo site

---
+++ _scripts/actions.js
@@ -0,0 +1,34 @@
+//MENU MOBILE
+
+let btnMenu = document.getElementById('btn-menu')
+let menu= document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+
+
+//SETA PARA SUBIR AUTOMATICAMENTE
+
+window.addEventListener('scroll', function () {
+    let scroll = document.querySelector('.scrollTop')
+    scroll.classList.toggle('active', window.scrollY > 450)
+})
+
+function backTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
